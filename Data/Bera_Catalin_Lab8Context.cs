d47c94522bb759986890caecfc05a3f10b04dd2a
Bera Catalin <catalin_bera@yahoo.com>
2022-01-12 15:43:37 +0200

    Lab8

---
+++ Data/Bera_Catalin_Lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Bera_Catalin_Lab8.Models;
+
+namespace Bera_Catalin_Lab8.Data
+{
+    public class Bera_Catalin_Lab8Context : DbContext
+    {
+        public Bera_Catalin_Lab8Context (DbContextOptions<Bera_Catalin_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bera_Catalin_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Bera_Catalin_Lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bera_Catalin_Lab8.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Book Title")]
+        public string Author { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
+        public int PublisherID { get; set; }
+        public Publisher Publisher { get; set; }
+        public ICollection<BookCategory> BookCategories { get; set; }
+
+    }
+}
